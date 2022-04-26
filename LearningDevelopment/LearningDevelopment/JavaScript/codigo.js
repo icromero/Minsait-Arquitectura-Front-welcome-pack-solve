807f3864d4a12ead403b409ffdff15b69afec314
icromero <102589367+icromero@users.noreply.github.com>
2022-04-26 17:33:00 +0200

    Delete LearningDevelopment/LearningDevelopment directory

--- LearningDevelopment/LearningDevelopment/JavaScript/codigo.js
+++
@@ -1,7 +0,0 @@
-// Spreed Operator in Object
-const obj1 = { firstName: 'Foo', age: 22 };
-const obj2 = { lastName: 'Bar', gender: 'M' };
-
-const newObj = {...obj1, ...obj2, planet: 'Earth' };
-
-console.log(newObj);
